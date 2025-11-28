d378b15db7251b0a846a2001d03cc6c293f3d99d
sheilawuor <sheilawuor01@gmail>
2025-11-28 23:53:47 +0300

    add changes

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+    
+    def match(self, possible_anagrams):
+        matches = []
+        word_sorted = sorted(self.word)
+        
+        for candidate in possible_anagrams:
+            if sorted(candidate.lower()) == word_sorted:
+                matches.append(candidate)
+        
+        return matches
